0db275451560577ede0e11dd2c67a1c7e4591c5e
Akuang Saechao <akuangsaechao@gmail.com>
2017-12-06 22:34:18 -0800

    Got GeoCode and Weather

--- app/src/main/java/akuangsaechao/volleyspot/AllSpotsMap.java
+++ app/src/main/java/akuangsaechao/volleyspot/AllSpotsMap.java
@@ -1,14 +1,18 @@
 package akuangsaechao.volleyspot;
 
 import android.database.Cursor;
+import android.hardware.Sensor;
+import android.hardware.SensorManager;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
@@ -43,12 +47,6 @@ public class AllSpotsMap extends AppCompatActivity implements OnMapReadyCallback
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
-
-
-
-        LatLng sydney = new LatLng(-34, 151);
-
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker Title").snippet("Marker Description"));
     }
 
     public void getAllVolleySpots() {
@@ -61,7 +59,7 @@ public class AllSpotsMap extends AppCompatActivity implements OnMapReadyCallback
                 String ID = c.getString(c.getColumnIndex(MyContentProvider._ID));
                 String title = c.getString(c.getColumnIndex(MyContentProvider.TITLE));
                 String address = c.getString(c.getColumnIndex(MyContentProvider.ADDRESS));
-                
+
             } while (c.moveToNext());
         }
     }
@@ -79,12 +77,6 @@ public class AllSpotsMap extends AppCompatActivity implements OnMapReadyCallback
         }
 
         @Override
-        protected void onCancelled() {
-            super.onCancelled();
-            this.cancel(true);
-        }
-
-        @Override
         protected StringBuilder doInBackground(Void... params) {
             try {
                 HttpURLConnection conn = null;
@@ -98,7 +90,6 @@ public class AllSpotsMap extends AppCompatActivity implements OnMapReadyCallback
                 while ((read = in.read(buff)) != -1) {
                     jsonResults.append(buff, 0, read);
                 }
-                String a = "";
                 return jsonResults;
             } catch (Exception e) {
                 e.printStackTrace();
@@ -117,18 +108,82 @@ public class AllSpotsMap extends AppCompatActivity implements OnMapReadyCallback
                 JSONObject before_geometry_jsonObj = resultJsonArray.getJSONObject(0);
                 JSONObject geometry_jsonObj = before_geometry_jsonObj.getJSONObject("geometry");
                 JSONObject location_jsonObj = geometry_jsonObj.getJSONObject("location");
+
                 String lat_helper = location_jsonObj.getString("lat");
-                double lat = Double.valueOf(lat_helper);
                 String lng_helper = location_jsonObj.getString("lng");
+
+                double lat = Double.valueOf(lat_helper);
                 double lng = Double.valueOf(lng_helper);
+
                 point = new LatLng(lat, lng);
 
                 mMap.addMarker(new MarkerOptions().position(point).title("Marker Title").snippet("Marker Description"));
+                CameraPosition cameraPosition = new CameraPosition.Builder().target(point).zoom(12).build();
+                mMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
 
             } catch (JSONException e) {
                 e.printStackTrace();
             }
         }
     }
+
+    public class WeatherAPI extends AsyncTask<Void, Void, StringBuilder> {
+        String place;
+
+        public WeatherAPI(String place) {
+            super();
+            this.place = place;
+        }
+
+        @Override
+        protected StringBuilder doInBackground(Void... params) {
+            try {
+                HttpURLConnection conn = null;
+                StringBuilder jsonResults = new StringBuilder();
+                String googleMapUrl = "http://api.openweathermap.org/data/2.5/weather?zip=" + place + ",us&units=imperial&APPID=7424bb17c1738362907a47e05e8686ee";
+                URL url = new URL(googleMapUrl);
+                conn = (HttpURLConnection) url.openConnection();
+                InputStreamReader in = new InputStreamReader(conn.getInputStream());
+                int read;
+                char[] buff = new char[1024];
+                while ((read = in.read(buff)) != -1) {
+                    jsonResults.append(buff, 0, read);
+                }
+                return jsonResults;
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return null;
+
+        }
+
+        @Override
+        protected void onPostExecute(StringBuilder result) {
+            super.onPostExecute(result);
+            try {
+
+                JSONObject jsonObj = new JSONObject(result.toString());
+                JSONObject location_jsonObj = jsonObj.getJSONObject("main");
+                String temperature = location_jsonObj.getString("temp");
+
+                mSensorManager = (SensorManager)getSystemService(SENSOR_SERVICE);
+                mTemperature= mSensorManager.getDefaultSensor(Sensor.TYPE_AMBIENT_TEMPERATURE);
+
+                
+
+                LatLng sydney = new LatLng(-34.852, 151.211);
+                mMap.addMarker(new MarkerOptions().position(sydney).title(temperature));
+                CameraPosition cameraPosition = new CameraPosition.Builder().target(sydney).zoom(12).build();
+                mMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
+
+
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private SensorManager mSensorManager;
+    private Sensor mTemperature;
 
 }
--- app/src/main/java/akuangsaechao/volleyspot/VolleySpotDetailFragment.java
+++ app/src/main/java/akuangsaechao/volleyspot/VolleySpotDetailFragment.java
@@ -1,5 +1,6 @@
 package akuangsaechao.volleyspot;
 
+import android.os.AsyncTask;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -14,6 +15,14 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 public class VolleySpotDetailFragment extends Fragment{
 
@@ -53,24 +62,14 @@ public class VolleySpotDetailFragment extends Fragment{
             @Override
             public void onMapReady(GoogleMap mMap) {
                 googleMap = mMap;
-
-                try {
-                    // For showing a move to my location button
-                    googleMap.setMyLocationEnabled(true);
-                } catch(SecurityException e){
-
-                }
-
-                // For dropping a marker at a point on the Map
-                LatLng sydney = new LatLng(-34, 151);
-                googleMap.addMarker(new MarkerOptions().position(sydney).title("Marker Title").snippet("Marker Description"));
-
-                // For zooming automatically to the location of the marker
-                CameraPosition cameraPosition = new CameraPosition.Builder().target(sydney).zoom(12).build();
-                googleMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
             }
         });
 
+        GeoCoderFromAddress geoCoderFromAddress = new GeoCoderFromAddress("6417 Aliso Way, Sacramento, CA 95828");
+        geoCoderFromAddress.execute();
+
+        WeatherAPI weatherAPI = new WeatherAPI("95112");
+        weatherAPI.execute();
 
         return rootView;
     }
@@ -88,9 +87,7 @@ public class VolleySpotDetailFragment extends Fragment{
     }
 
     public void updateArticleView(int position) {
-
         if (mCurrentPosition != position) {
-
             mCurrentPosition = position;
         }
     }
@@ -101,4 +98,112 @@ public class VolleySpotDetailFragment extends Fragment{
         outState.putInt(ARG_POSITION, mCurrentPosition);
     }
 
+    public class GeoCoderFromAddress extends AsyncTask<Void, Void, StringBuilder> {
+        String place;
+
+        public GeoCoderFromAddress(String place) {
+            super();
+            this.place = place;
+        }
+
+        @Override
+        protected StringBuilder doInBackground(Void... params) {
+            try {
+                HttpURLConnection conn = null;
+                StringBuilder jsonResults = new StringBuilder();
+                String googleMapUrl = "http://maps.googleapis.com/maps/api/geocode/json?address=" + this.place + "&sensor=false";
+                URL url = new URL(googleMapUrl);
+                conn = (HttpURLConnection) url.openConnection();
+                InputStreamReader in = new InputStreamReader(conn.getInputStream());
+                int read;
+                char[] buff = new char[1024];
+                while ((read = in.read(buff)) != -1) {
+                    jsonResults.append(buff, 0, read);
+                }
+                return jsonResults;
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return null;
+
+        }
+
+        @Override
+        protected void onPostExecute(StringBuilder result) {
+            super.onPostExecute(result);
+            try {
+
+                JSONObject jsonObj = new JSONObject(result.toString());
+                JSONArray resultJsonArray = jsonObj.getJSONArray("results");
+                JSONObject before_geometry_jsonObj = resultJsonArray.getJSONObject(0);
+                JSONObject geometry_jsonObj = before_geometry_jsonObj.getJSONObject("geometry");
+                JSONObject location_jsonObj = geometry_jsonObj.getJSONObject("location");
+
+                String lat_helper = location_jsonObj.getString("lat");
+                String lng_helper = location_jsonObj.getString("lng");
+
+                double lat = Double.valueOf(lat_helper);
+                double lng = Double.valueOf(lng_helper);
+
+                LatLng point = new LatLng(lat, lng);
+
+                googleMap.addMarker(new MarkerOptions().position(point).title("Marker Title").snippet("Marker Description"));
+                CameraPosition cameraPosition = new CameraPosition.Builder().target(point).zoom(12).build();
+                googleMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
+
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public class WeatherAPI extends AsyncTask<Void, Void, StringBuilder> {
+        String place;
+
+        public WeatherAPI(String place) {
+            super();
+            this.place = place;
+        }
+
+        @Override
+        protected StringBuilder doInBackground(Void... params) {
+            try {
+                HttpURLConnection conn = null;
+                StringBuilder jsonResults = new StringBuilder();
+                String googleMapUrl = "http://api.openweathermap.org/data/2.5/weather?zip=" + place + ",us&units=imperial&APPID=7424bb17c1738362907a47e05e8686ee";
+                URL url = new URL(googleMapUrl);
+                conn = (HttpURLConnection) url.openConnection();
+                InputStreamReader in = new InputStreamReader(conn.getInputStream());
+                int read;
+                char[] buff = new char[1024];
+                while ((read = in.read(buff)) != -1) {
+                    jsonResults.append(buff, 0, read);
+                }
+                return jsonResults;
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return null;
+
+        }
+
+        @Override
+        protected void onPostExecute(StringBuilder result) {
+            super.onPostExecute(result);
+            try {
+
+                JSONObject jsonObj = new JSONObject(result.toString());
+                JSONObject location_jsonObj = jsonObj.getJSONObject("main");
+                String temperature = location_jsonObj.getString("temp");
+
+                LatLng sydney = new LatLng(-34.852, 151.211);
+                googleMap.addMarker(new MarkerOptions().position(sydney).title(temperature));
+                CameraPosition cameraPosition = new CameraPosition.Builder().target(sydney).zoom(12).build();
+                googleMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
+
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 }
